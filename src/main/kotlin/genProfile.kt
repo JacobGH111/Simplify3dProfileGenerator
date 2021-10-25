b259641e826dbcae8958f99be5c68dffffb180a8
Jacob Heller <jacobgheller@gmail.com>
2021-10-24 20:29:46 -0400

    fixes

--- src/main/kotlin/genProfile.kt
+++ src/main/kotlin/genProfile.kt
@@ -3,7 +3,6 @@ import java.io.File
 import java.io.StringReader
 import java.math.BigDecimal
 import java.math.RoundingMode
-import java.time.Duration
 import javax.xml.parsers.DocumentBuilderFactory
 
 
@@ -19,11 +18,11 @@ fun main(args: Array<String>) {
 data class Nozzle(val diameter: Double, val extruderTempOffSet: Int = 0, val defaultSpeedMultiplier: Double = 1.0)
 
 val nozzles = listOf(
-        Nozzle(diameter = 0.1),
-        Nozzle(diameter = 0.2),
-        Nozzle(diameter = 0.4),
-        Nozzle(diameter = 0.6),
-        Nozzle(diameter = 1.0, extruderTempOffSet = 30, defaultSpeedMultiplier = .5)
+//        Nozzle(diameter = 0.1),
+//        Nozzle(diameter = 0.2),
+        Nozzle(diameter = 0.4)
+//        Nozzle(diameter = 0.6),
+//        Nozzle(diameter = 1.0, extruderTempOffSet = 30, defaultSpeedMultiplier = .5)
 )
 
 
@@ -43,9 +42,9 @@ data class Material(val name: String,
                     val firstLayerWidthPercentage: Int = 110
 )
 
+val plaFirstWidth = 130
 val materials = listOf(
-        Material(name = "PLA", extruderTemp = 210, bedTemp = 60, extrusionMultiplier = 1.05, defaultPrintingSpeed = 60),
-        Material(name = "PLA+", extruderTemp = 210, bedTemp = 60, extrusionMultiplier = 1.05, defaultPrintingSpeed = 60),
+        Material(name = "PLA/PLA+", extruderTemp = 215, bedTemp = 60, extrusionMultiplier = 1.05, defaultPrintingSpeed = 60, firstLayerWidthPercentage = plaFirstWidth),
         Material(name = "PETG", extruderTemp = 250, bedTemp = 70, extrusionMultiplier = 1.10, defaultPrintingSpeed = 60),
         Material(name = "TPU", extruderTemp = 220, bedTemp = 60, extrusionMultiplier = 1.0, defaultPrintingSpeed = 30, firstLayerHeightPercentage = 200, firstLayerWidthPercentage = 75)
 )
@@ -68,11 +67,12 @@ fun genQuality(name: String, layerHeight: Double, infillPercntage: Int): String 
 fun genMaterial(material: Material, nozzle: Nozzle): String {
     val heaterRecoveryTime = if( (material.extruderTemp + nozzle.extruderTempOffSet) <= 250 ) 45 else 90
 
+    val purgeZHeight = "0.5"
     return """<autoConfigureMaterial name="${material.name}">
     <globalExtruderTemperature>${material.extruderTemp + nozzle.extruderTempOffSet}</globalExtruderTemperature>
     <globalBedTemperature>${material.bedTemp}</globalBedTemperature>
     <globalExtrusionMultiplier>${material.extrusionMultiplier}</globalExtrusionMultiplier>
-     <startingGcode>G92 E0 ; Reset Extruder distance to 0,G1 E-3.5 ; Retracts filament to prevent blobs during probing,G28 ; home all axes,G28 Z ; home Z to get more accurate Z position,G29; EZABL mesh generation,G4 S$heaterRecoveryTime; wait for heaters to recover,M117 Purge extruder,G92 E0 ; reset extruder,G1 Z1.0 F3000 ; move z up little,G1 X0.1 Y20 Z0.3 F5000.0 ; move to start-line position,G1 X0.1 Y100.0 Z0.3 F750.0 E15 ; draw 1st line,G1 X0.4 Y100.0 Z0.3 F5000.0 ; move to side a little,G1 X0.4 Y20 Z0.3 F750.0 E30 ; draw 2nd line,G92 E0 ; reset extruder,G1 Z1.0 F3000 ; move z up little,M117 Printing.....</startingGcode>
+     <startingGcode>G92 E0 ; Reset Extruder distance to 0,G1 E-3.5 ; Retracts filament to prevent blobs during probing,G28 ; home all axes,G28 Z ; home Z to get more accurate Z position,G29; EZABL mesh generation,G4 S$heaterRecoveryTime; wait for heaters to recover,M117 Purge extruder,G92 E0 ; reset extruder,G1 Z1.0 F3000 ; move z up little,G1 X0.1 Y20 Z$purgeZHeight F5000.0 ; move to start-line position,G1 X0.1 Y100.0 Z$purgeZHeight F750.0 E15 ; draw 1st line,G1 X0.4 Y100.0 Z$purgeZHeight F5000.0 ; move to side a little,G1 X0.4 Y20 Z$purgeZHeight F750.0 E30 ; draw 2nd line,G92 E0 ; reset extruder,G1 Z1.0 F3000 ; move z up little,M117 Printing.....</startingGcode>
     <fanSpeed>
       <setpoint layer="1" speed="100"/>
     </fanSpeed>
