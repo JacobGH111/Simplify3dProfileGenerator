32e3adc164f7baeb95f83913dc7d649faa9c9520
Jacob Heller <jacobgheller@gmail.com>
2021-12-05 18:52:32 -0500

    update extrusion settings for new extruder

--- src/main/kotlin/Materials.kt
+++ src/main/kotlin/Materials.kt
@@ -1,9 +1,9 @@
 
 
-val plaFirstWidth = 130
+val plaFirstWidth = 100
 val materials = listOf(
         Material(name = "PLA/PLA+", extruderTemp = 218, bedTemp = 60, extrusionMultiplier = 1.0, defaultPrintingSpeed = 60, firstLayerWidthPercentage = plaFirstWidth),
-        Material(name = "PETG", extruderTemp = 250, bedTemp = 70, extrusionMultiplier = 1.0, defaultPrintingSpeed = 60),
+        Material(name = "PETG", extruderTemp = 250, bedTemp = 70, extrusionMultiplier = 1.0, defaultPrintingSpeed = 60, firstLayerWidthPercentage = 100),
         Material(name = "TPU", extruderTemp = 220, bedTemp = 60, extrusionMultiplier = 1.0, defaultPrintingSpeed = 30, firstLayerHeightPercentage = 200, firstLayerWidthPercentage = 75)
 )
 
@@ -13,5 +13,5 @@ data class Material(val name: String,
                     val extrusionMultiplier: Double,
                     val defaultPrintingSpeed: Int,
                     val firstLayerHeightPercentage: Int = 90,
-                    val firstLayerWidthPercentage: Int = 110
+                    val firstLayerWidthPercentage: Int
 )
