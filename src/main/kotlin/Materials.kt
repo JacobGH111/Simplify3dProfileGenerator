ade528bf73c40ac02daa2c3232dee13fbd688528
Jacob Heller <jacobgheller@gmail.com>
2022-06-26 12:33:59 -0400

    changed TPU temperature from 220 to 200

--- src/main/kotlin/Materials.kt
+++ src/main/kotlin/Materials.kt
@@ -4,7 +4,7 @@ val plaFirstWidth = 100
 val materials = listOf(
         Material(name = "PLA/PLA+", extruderTemp = 218, bedTemp = 60, extrusionMultiplier = 1.0, defaultPrintingSpeed = 60, firstLayerWidthPercentage = plaFirstWidth),
         Material(name = "PETG", extruderTemp = 250, bedTemp = 70, extrusionMultiplier = 1.0, defaultPrintingSpeed = 60, firstLayerWidthPercentage = 100),
-        Material(name = "TPU", extruderTemp = 220, bedTemp = 60, extrusionMultiplier = 1.0, defaultPrintingSpeed = 30, firstLayerHeightPercentage = 200, firstLayerWidthPercentage = 75)
+        Material(name = "TPU", extruderTemp = 200, bedTemp = 60, extrusionMultiplier = 1.0, defaultPrintingSpeed = 30, firstLayerHeightPercentage = 200, firstLayerWidthPercentage = 75)
 )
 
 data class Material(val name: String,
